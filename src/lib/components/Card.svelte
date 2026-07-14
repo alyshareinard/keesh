2ae0c2c4ecc047326ceb6d6f9f961b8ea7a380bd
Alysha Reinard <alysha.reinard@gmail.com>
2026-07-14 10:40:38 +0200

    mt-3 on flex container - may need to adjust padding on symbols themselves

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -29,8 +29,8 @@
 	<span class="absolute top-1 left-1 text-sm sm:text-xs font-bold {colorClass}">
 		{card.rank}{symbol}
 	</span>
-<div class="flex flex-col items-center mt-4" >
-	<span class="text-4xl {colorClass}">{symbol}</span>
+<div class="flex flex-col items-center mt-3" >
+	<span class="text-3xl {colorClass}">{symbol}</span>
 	<span class="text-2xl font-bold {colorClass}">{card.rank}</span>
 </div>
 	<span class="absolute bottom-1 right-1 text-sm sm:text-xs font-bold {colorClass} rotate-180">
